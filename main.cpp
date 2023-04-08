17a15643e08c1df570152d3530f68cc3e6282292
killianharr <killianh@live.ie>
2023-04-08 21:15:47 +0000

    using simple loops
    Signed-off-by: killianharr <killianh@live.ie>

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
 #include "mbed.h"
 
-DigitalOut myled(LED1);
+int nth_term=7;
+int term_1, term_2;
+int next_term = 0;
 
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
+    
+    printf("lab 2 program \r\n");
+    term_1 = 0;
+    term_2 = 1;
+
+    printf("nth trem is %d", nth_term);
+    printf("0, 1, ");
+    for(int i=2; i<nth_term; i++){
+        next_term = term_1 + term_2;
+        printf("%d, ", next_term);
+        term_1 = term_2;
+        term_2 =next_term;
   }
+  printf("..!\r\n");
 }
